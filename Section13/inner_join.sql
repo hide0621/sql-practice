14190e0eddd8a6ffc6a5b64cec3f9648070e77a2
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-05-11 11:23:08 +0900

    left join,right join, full joinについて学習した

--- Section13/inner_join.sql
+++ Section13/inner_join.sql
@@ -24,3 +24,27 @@ on std.first_name = usr.first_name and std.last_name = usr.last_name;
 select * from employees as emp
 inner join students as std 
 on emp.id < std.id;
+
+-- left joinの勉強
+select emp.id, emp.first_name, emp.last_name, dt.id as department_id, dt.name as department_name 
+from employees as emp
+left join departments as dt 
+on emp.department_id = dt.id;
+
+-- right joinの勉強
+select * from students as std
+left join enrollments as enr
+on std.id = enr.student_id
+left join classes as cs 
+on enr.class_id = cs.id;
+
+select * from students as std
+right join enrollments as enr
+on std.id = enr.student_id
+right join classes as cs 
+on enr.class_id = cs.id;
+
+-- full joinの練習（両方から取り出して、値が取り出せないものはnullとする）
+select * from enrollments as enr
+full join classes as cs 
+on enr.class_id = cs.id;
