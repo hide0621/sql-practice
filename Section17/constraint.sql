164433d71e2f92b6b6831adb72c205f382384b9e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-05-24 13:05:55 +0900

    後からdefault制約、not null制約,check制約を追加する方法を学んだ

--- Section17/constraint.sql
+++ Section17/constraint.sql
@@ -20,4 +20,30 @@ alter table employees drop constraint uniq_employees_name;
 -- 後からunique制約をつけた
 alter table employees add constraint uniq_employees_name_age unique(name, age);
 
+-- defaultの追加
+
+select * from customers;
+
+-- check制約の削除
+alter table customers drop constraint customers_age_check; 
+
+-- defaultの追加
+alter table customers alter age set default 20;
+insert into customers (id, name) values (2, 'Jiro');
+select * from customers;
+
+-- not nullの追加
+ALTER TABLE customers
+ALTER COLUMN name TYPE varchar(255),
+ALTER COLUMN name SET NOT NULL;
+
+insert into customers (id, name) values (3, null);
+
+-- check制約の追加
+alter table customers add constraint check_age check(age >= 20);
+
+
+
+
+
 
